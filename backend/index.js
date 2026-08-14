41411fb5ef2fae55df2577a36ba817b39aed1540
unknown <gbrljlima@gmail.com>
2026-08-14 14:24:21 -0300

    Feat: Modelo Tarefas

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const cors = require('cors');
+
+app.use(express.json());
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
+const allowedOrigins = ['http://localhost:5173',
+    'http://localhost:3001'];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            console.log('Origin:', origin);
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
---
+++ backend/models/TaskModel.js
@@ -0,0 +1,39 @@
+//  Tela principal que exibe todas as tarefas e permite interações.
+//  Mostrar título, descrição e status (concluída / pendente)
+// • Botão para alternar status (concluída/pendente)
+// • Botão para excluir (com confirmação)
+//  Formulário para adicionar e editar tarefas.
+
+
+let tarefas = [];
+let proximoId = 1;
+
+const STATUS = ['Concluída', 'Pendente'];
+
+module.exports = {
+    listarTarefas: () => {
+        return tarefas;
+    },
+    buscarId: (id) => {
+        return tarefas.find(tarefa => tarefa.id === id);
+    },
+    criar: (titulo, descricao) => {
+        const novaTarefa = {
+            id: proximoId++, 
+            titulo, 
+            descricao,
+            status:'Pendente'
+        };
+        tarefas.push(novaTarefa);
+        return novaTarefa;
+    },
+    atualizar: (id, novosDados) => {
+        const indice = tarefas.findIndex(tarefa => tarefa.id === id);
+        tarefas[indice] = {...tarefas[indice], ... novosDados};
+        return tarefas[indice]; 
+    },
+    excluir: (id) => {
+        const indice = tarefas.findIndex(tarefa => tarefa.id === id);
+        return tarefas.splice(indice, 1)[0];
+    }
+}
