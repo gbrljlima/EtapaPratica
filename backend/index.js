b53c39cdff37575d0414634c8325c02b9532021c
unknown <gbrljlima@gmail.com>
2026-08-16 19:13:15 -0300

    Fix: Mudança CORS

--- backend/index.js
+++ backend/index.js
@@ -4,23 +4,10 @@ const port = 3001;
 const cors = require('cors');
 const taskRouter = require('./routes/TaskRoute');
 
-// const allowedOrigins = ['http://localhost:5173',
-//     'http://localhost:3001', 
-//     'https://gerenciador-de-tarefas-ysd6.onrender.com', 
-//     'https://gerenciador-de-tarefas-drab-xi.vercel.app'];
+const allowedOrigins = ['http://localhost:5173',
+    'https://gerenciador-de-tarefas-drab-xi.vercel.app'];
 
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//             callback(null, true);
-//         } else {
-//             console.log('Origin:', origin);
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-// };
-
-app.use(cors());
+app.use(cors({origin: allowedOrigins}));
 app.use(express.json());
 app.use(taskRouter);
 app.listen(port, () => {
