98b65fbcded655a0d2919251cfbb16859894722b
unknown <gbrljlima@gmail.com>
2026-08-16 18:02:53 -0300

    Fix: Render URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:3001/api/tasks';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api/tasks';
 
 
 export const criarTarefa = async (tarefa) => {
